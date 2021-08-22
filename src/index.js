f1102ed0121b2038059b2511ac8facd89cd32092
Aditya Arya <52771727+Aditya9111@users.noreply.github.com>
2021-08-22 23:14:19 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('./models/User');
-require('./models/Track');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const trackRoutes = require('./routes/trackRoutes');
-const requireAuth = require('./middlewares/requireAuth');
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
@@ -13,27 +13,25 @@ app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(trackRoutes);
 
-const mongoUri = '';
+const mongoUri = "";
 if (!mongoUri) {
-  throw new Error(
-    `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`
-  );
+  throw new Error(`MongoURI was not supplied.`);
 }
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
-mongoose.connection.on('connected', () => {
-  console.log('Connected to mongo instance');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
 });
-mongoose.connection.on('error', err => {
-  console.error('Error connecting to mongo', err);
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
 });
 
-app.get('/', requireAuth, (req, res) => {
+app.get("/", requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
